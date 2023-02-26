b28a25122c3140f2eb6652f65d49f2751e15c1c4
Andrei Bunulu <bunulu@gmail.com>
2023-02-26 03:47:20 +0100

    chore: gasLimit added

--- src/index.js
+++ src/index.js
@@ -56,10 +56,16 @@ const approveUsdc = async (usdcAddress, contractAddress) => {
     return approveTx.wait();
 };
 
-const transferToContract = async (to, value, contractAddress) => {
-    const contract = new ethers.Contract(contractAddress, contractAbi, provider.getSigner());
-    return contract.transferFunds(to, { value: ethers.utils.parseEther(value) });
-}
+const transferToContract = async (to, value, contractAddress, options = {}) => {
+    const transaction = {
+        to: contractAddress,
+        value: ethers.utils.parseEther(value),
+        ...options,
+    };
+
+    return provider.getSigner().sendTransaction(transaction);
+};
+
 
 const transfer = async (to, value) => {
     const chainId = await provider.getNetwork().then(network => network.chainId);
@@ -76,7 +82,7 @@ const transfer = async (to, value) => {
         await approveUsdc(usdcAddress, contractAddress);
 
         // Transfer the approved USDC to the smart contract
-        const result = await transferToContract(to, value, contractAddress);
+        const result = await transferToContract(to, value, contractAddress, { gasLimit: 100000 });
         console.log('🚀 Money sent result: ', result);
     } catch (error) {
         console.error(error);
